125adeeff0c292aa81b6a075f5a61ef08303cb32
Fellipe Utaka <fellipeutaka@gmail.com>
2024-06-10 12:34:40 -0300

    feat: add loading to server version combobox

--- src/components/servers/new-server-form.tsx
+++ src/components/servers/new-server-form.tsx
@@ -83,17 +83,33 @@ export function NewServerForm() {
                             (version) => version.id === field.value,
                           )?.id
                         : "Select version"}
-                      <Icons.ChevronDown className="ml-2 size-4 shrink-0 opacity-50 transition-transform duration-200 group-aria-expanded:rotate-180" />
+                      <Icons.ChevronDown className="ml-2 size-4 opacity-50 transition-transform duration-200 group-aria-expanded:rotate-180" />
                     </Button>
                   </Form.Control>
                 </Popover.Trigger>
-                <Popover.Content className="w-[--radix-popper-anchor-width] p-0">
+                <Popover.Content
+                  avoidCollisions={false}
+                  side="bottom"
+                  className="w-[--radix-popper-anchor-width] p-0"
+                >
                   <Command>
                     <Command.Input
                       placeholder="Search version..."
                       className="h-9"
+                      disabled={isLoading}
                     />
-                    <Command.Empty>Version not found.</Command.Empty>
+                    <Command.Empty>
+                      {isLoading ? (
+                        <div className="flex justify-center items-center gap-2">
+                          <Icons.Loader className="animate-spin size-4" />
+                          Fetching versions...
+                        </div>
+                      ) : isError ? (
+                        "Failed to fetch versions."
+                      ) : (
+                        "Version not found."
+                      )}
+                    </Command.Empty>
                     <Command.Group>
                       <Command.List>
                         {versions?.map((version) => (
@@ -101,7 +117,9 @@ export function NewServerForm() {
                             value={version.id}
                             key={version.id}
                             onSelect={() => {
-                              form.setValue("version", version.id);
+                              form.setValue("version", version.id, {
+                                shouldValidate: true,
+                              });
                               setIsVersionOpen(false);
                             }}
                           >
--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -26,6 +26,24 @@ const SocialIcons = {
 
 export const Icons = {
   ...SocialIcons,
+  Loader: (props) => (
+    <svg
+      role="img"
+      aria-label="Loader"
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M12 2v4M16.2 7.8l2.9-2.9M18 12h4M16.2 16.2l2.9 2.9M12 18v4M4.9 19.1l2.9-2.9M2 12h4M4.9 4.9l2.9 2.9" />
+    </svg>
+  ),
   X: (props) => (
     <svg
       role="img"
